8ae9997e628fb6d10e8db3a3ef97f0f7c9cff7ad
Juan Cruz <xuan@sal.xuan.com.ar>
2016-12-19 14:04:09 +0100

    Added logging

--- src/main/java/com/xuan/urlshortener/UrlResource.java
+++ src/main/java/com/xuan/urlshortener/UrlResource.java
@@ -8,8 +8,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Path("/")
 public class UrlResource {
+
+    private final static Logger LOG = LoggerFactory.getLogger(UrlResource.class);
 
     private final String shortDomain;
 
@@ -20,12 +25,14 @@ public class UrlResource {
     @GET
     @Path("/{key}")
     public Response redirect(@PathParam("key") String key) {
+        LOG.debug("Requesting URL key {}", key);
         String url = "http://google.com/search?q=" + key;
         return Response.temporaryRedirect(URI.create(url)).build();
     }
 
     @POST
     public Response create(String longUrl) {
+        LOG.debug("Creating new short URL for {}", longUrl);
         String key = "test";
         String shortUrl = String.format("http://%s/%s", shortDomain, key);
         return Response.created(URI.create(shortUrl)).build();
